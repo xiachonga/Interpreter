e0993bdab88cc1570d359e7212ccf3267c23d81a
张忠诚 <zhangzhongcheng@zhangzhongchengdeMacBook-Pro.local>
2020-10-08 11:44:45 +0800

    格式化了源码

--- Interpreter/ast-interpreter/ASTInterpreter.cpp
+++ Interpreter/ast-interpreter/ASTInterpreter.cpp
@@ -17,75 +17,91 @@ using namespace clang;
 
 #include "Environment.h"
 //zzctest
-class InterpreterVisitor : 
-   public EvaluatedExprVisitor<InterpreterVisitor> {
+class InterpreterVisitor : public EvaluatedExprVisitor<InterpreterVisitor>
+{
 public:
-   explicit InterpreterVisitor(const ASTContext &context, Environment * env)
-   : EvaluatedExprVisitor(context), mEnv(env) {}
-   virtual ~InterpreterVisitor() {}
+    explicit InterpreterVisitor(const ASTContext &context, Environment *env)
+        : EvaluatedExprVisitor(context), mEnv(env) {}
+    virtual ~InterpreterVisitor() {}
 
-   virtual void VisitIntegerLiteral (IntegerLiteral * integer) {
-      mEnv->integerLiteral(integer);
-   }
-   virtual void VisitBinaryOperator (BinaryOperator * bop) {
-	   VisitStmt(bop);
-	   mEnv->binop(bop);
-   }
-   virtual void VisitDeclRefExpr(DeclRefExpr * expr) {
-	   VisitStmt(expr);
-	   mEnv->declref(expr);
-   }
-   virtual void VisitCastExpr(CastExpr * expr) {
-	   VisitStmt(expr);
-	   mEnv->cast(expr);
-   }
-   virtual void VisitCallExpr(CallExpr * call) {
-	   VisitStmt(call);
-	   mEnv->call(call);
-   }
-   virtual void VisitDeclStmt(DeclStmt * declstmt) {
-	   mEnv->decl(declstmt);
-   }
+    virtual void VisitIntegerLiteral(IntegerLiteral *integer)
+    {
+        mEnv->integerLiteral(integer);
+    }
+    virtual void VisitBinaryOperator(BinaryOperator *bop)
+    {
+        VisitStmt(bop);
+        mEnv->binop(bop);
+    }
+    virtual void VisitDeclRefExpr(DeclRefExpr *expr)
+    {
+        VisitStmt(expr);
+        mEnv->declref(expr);
+    }
+    virtual void VisitCastExpr(CastExpr *expr)
+    {
+        VisitStmt(expr);
+        mEnv->cast(expr);
+    }
+    virtual void VisitCallExpr(CallExpr *call)
+    {
+        VisitStmt(call);
+        mEnv->call(call);
+    }
+    virtual void VisitDeclStmt(DeclStmt *declstmt)
+    {
+        mEnv->decl(declstmt);
+    }
+
 private:
-   Environment * mEnv;
+    Environment *mEnv;
 };
 
-class InterpreterConsumer : public ASTConsumer {
+class InterpreterConsumer : public ASTConsumer
+{
 public:
-   explicit InterpreterConsumer(const ASTContext& context) : mEnv(),
-   	   mVisitor(context, &mEnv) {
-   }
-   virtual ~InterpreterConsumer() {}
+    explicit InterpreterConsumer(const ASTContext &context) : mEnv(),
+                                                              mVisitor(context, &mEnv)
+    {
+    }
+    virtual ~InterpreterConsumer() {}
 
-   virtual void HandleTranslationUnit(clang::ASTContext &Context) {
-	   TranslationUnitDecl * decl = Context.getTranslationUnitDecl();
-	   mEnv.init(decl);
+    virtual void HandleTranslationUnit(clang::ASTContext &Context)
+    {
+        TranslationUnitDecl *decl = Context.getTranslationUnitDecl();
+        mEnv.init(decl);
 
-	   FunctionDecl * entry = mEnv.getEntry();
-	   mVisitor.VisitStmt(entry->getBody());
-  }
+        FunctionDecl *entry = mEnv.getEntry();
+        mVisitor.VisitStmt(entry->getBody());
+    }
+
 private:
-   Environment mEnv;
-   InterpreterVisitor mVisitor;
+    Environment mEnv;
+    InterpreterVisitor mVisitor;
 };
 
-class InterpreterClassAction : public ASTFrontendAction {
-public: 
-  virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
-    clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
-    return std::unique_ptr<clang::ASTConsumer>(
-        new InterpreterConsumer(Compiler.getASTContext()));
-  }
+class InterpreterClassAction : public ASTFrontendAction
+{
+public:
+    virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+        clang::CompilerInstance &Compiler, llvm::StringRef InFile)
+    {
+        return std::unique_ptr<clang::ASTConsumer>(
+            new InterpreterConsumer(Compiler.getASTContext()));
+    }
 };
 
-int main (int argc, char ** argv) {
-   if (argc > 1) {
-      ifstream ifile(argv[1]);
-      ostringstream buf;
-      char ch;
-      while(buf&&ifile.get(ch)) {
-         buf.put(ch);
-      }
-      clang::tooling::runToolOnCode(std::unique_ptr<clang::FrontendAction>(new InterpreterClassAction), buf.str());
-   }
+int main(int argc, char **argv)
+{
+    if (argc > 1)
+    {
+        ifstream ifile(argv[1]);
+        ostringstream buf;
+        char ch;
+        while (buf && ifile.get(ch))
+        {
+            buf.put(ch);
+        }
+        clang::tooling::runToolOnCode(std::unique_ptr<clang::FrontendAction>(new InterpreterClassAction), buf.str());
+    }
 }
